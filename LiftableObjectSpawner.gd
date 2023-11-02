a9521d7be2d5fcc12ec043bf2d74dbdd75919336
lautaro-estra <elautaro632@gmail.com>
2023-11-01 23:28:14 -0300

    creo que ya estaria

--- LiftableObjectSpawner.gd
+++ LiftableObjectSpawner.gd
@@ -1,6 +1,7 @@
 extends Marker3D
+@export var size:float=1
 
-var chair_scene : PackedScene = preload("res://chair.tscn")
+@export var chair_scene : PackedScene = preload("res://chair.tscn")
 var chair_instance : Node
 var spawn_timer : Timer
 var check_timer : Timer
@@ -10,9 +11,10 @@ var replacement_timer_duration : float = 5
 func _ready():
 	spawn_timer = Timer.new()
 	check_timer = Timer.new()
+func setup_timers():
 
 	spawn_timer.wait_time = 10
-	check_timer.wait_time = 1
+	check_timer.wait_time = 5
 
 	spawn_timer.one_shot = true
 	check_timer.one_shot = true
@@ -20,13 +22,19 @@ func _ready():
 	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
 	check_timer.timeout.connect(_on_check_timer_timeout)
 
-	spawn_timer.start()
+	#spawn_timer.start()
 	check_timer.start()
-
+	spawn_chair()
+	
 func spawn_chair():
 	if chair_scene:
 		chair_instance = chair_scene.instance()
+		if chair_instance.has_method("set_size"):
+			chair_instance.set_size(size)
+		#chair_scene.size= size
+		queue_free_chair()
 		add_child(chair_instance)
+		spawn_timer.start()
 
 func _on_spawn_timer_timeout():
 	spawn_chair()
@@ -34,6 +42,7 @@ func _on_spawn_timer_timeout():
 
 func _on_check_timer_timeout():
 	if !chair_instance:
+		
 		return
 
 	if !chair_instance.is_inside_tree():
@@ -52,7 +61,7 @@ func _on_check_timer_timeout():
 		if !player_found:
 			spawn_timer.wait_time = replacement_timer_duration
 			spawn_timer.start()
-			check_timer.stop()
+			check_timer.start()
 
 func queue_free_chair():
 	if chair_instance:
