27682a84f119138abdc5bcf7aa83c1036950bdd7
0horane <lehorane@gmail.com>
2023-11-04 03:36:30 -0300

    cambio, funciona en android ahora

--- CoreScenes/Camera/Camera3D.gd
+++ CoreScenes/Camera/Camera3D.gd
@@ -7,6 +7,14 @@ var camera_speed = 2.0
 func _ready():
 	player = $"../Player"  
 
+
+# https://www.youtube.com/watch?v=u2sX8UhGkoQ
+# Aca esta un video de como era el katamari móvil con acelerometro
+# No te dejaba ir para atrás, para no causar problemas de cámara como tenemos ahora, sino que te 
+# permitía dar una vuelta total mirando para atrás. Podríamos replicarlo creo.
+
+# Ademas, hay que hacer que la camara siga a una altura y distancia constante de la esfera según 
+# tamaño. Sin tener en cuenta la altura variable casi no vibraría, me parece.  
 func _process(delta):
 	if is_instance_valid(player):
 		camera_offset = Vector3(0, 2, -5).rotated(Vector3(0,1,0), player.movementRotation)
